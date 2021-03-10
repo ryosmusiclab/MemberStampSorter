b4a8bb2dfe4343a59b4fb35c3a9c81bdd66d16f9
ryosmusiclab <ryosmusiclab@gmail.com>
2021-03-10 11:40:05 +0900

    Initial commit

---
+++ src/content_script.js
@@ -0,0 +1,59 @@
+var timer;
+$(window).on('load', () => {
+  console.log("loaded");
+  document.querySelector("#input-panel #buttons #button").click();
+  document.querySelector("#input-panel #buttons #button").click();
+  channelId = getChannelId();
+  console.log(channelId);
+  
+  chrome.storage.local.get(function (items) {
+    order = items[channelId];
+    if (order != null) {
+      console.log(order);
+      timer = setInterval(function(){
+        result = applyOrder(order);
+        console.log(result,order.length)
+        if(result == order.length){
+          console.log("finished.");
+          clearInterval(timer);
+        }
+      }, 500);
+    }else{
+      console.log("not stored");
+      console.log(items);
+    }
+  });
+});
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  //console.log(msg);
+  if (msg.operation == "getOrder") {
+    container = document.querySelector("div#categories div#emoji");
+    array = [].slice.call(container.querySelectorAll("img"))
+      .map(function (v) {
+        src = v.getAttribute('src');
+        label = v.getAttribute('aria-label');
+        return { src: src, label: label }
+      });
+    sendResponse(array);
+
+  } else if (msg.operation == "applyOrder") {
+    order = msg.order;
+    console.log(order);
+    applyOrder(order);
+    sendResponse("done");
+  } else if (msg.operation == "getChid") {
+    sendResponse(getChannelId());
+  }
+});
+
+function applyOrder(order) {
+  container = document.querySelector("div#categories div#emoji");
+  array = [].slice.call(container.querySelectorAll("img"));
+  array.sort(function (a, b) {
+    return order.indexOf(a.getAttribute("aria-label")) - order.indexOf(b.getAttribute("aria-label"));
+  }).forEach(function (v) { container.append(v) });
+  return array.length;
+}
+function getChannelId(){
+  return document.querySelector("#input-panel #category-buttons").firstElementChild.id;
+}
---
+++ src/popup.js
@@ -0,0 +1,60 @@
+var stampCount=0;
+var timer;
+var channelId = "";
+$(window).on('load', () => {
+    getChannelId();
+    waitforStamps();
+    timer = setInterval(waitforStamps, 500);
+});
+$("#applyBtn").on("click", () => {
+    order = getOrder();
+    applyOrder(order);
+});
+function waitforStamps(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { operation: "getOrder" }, function (response) {
+            console.log(stampCount,response.length);
+            if(stampCount == response.length && stampCount > 0){
+                clearInterval(timer);
+            }else{
+                stampCount = response.length;
+            }
+            displayStamps(response);
+        });
+    });
+}
+
+function displayStamps(images) {
+    document.querySelector("#stamplist").innerHTML = "";
+    images.forEach(function (v) {
+        console.log(v.src);
+        tmp = document.createElement("img");
+        tmp.src = v.src;
+        tmp.id = v.label;
+        tmp.draggable = "true";
+        $('#stamplist').append(tmp);
+    });
+    $('#stamplist').sortable({
+        opacity: 0.5,
+        placeholder: "ph1"
+    });
+
+}
+function applyOrder(order){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { operation: "applyOrder",order: order},function (response) {
+            console.log(response);
+        });
+    });
+    chrome.storage.local.set({[channelId]: order}, function() {console.log('stored to '+channelId);});
+}
+function getOrder(){
+    return $("#stamplist").sortable("toArray");
+}
+function getChannelId(){
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { operation: "getChid" }, function (response) {
+            channelId = response;
+        });
+    });
+}
